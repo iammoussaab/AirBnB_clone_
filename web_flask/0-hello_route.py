960730448840631d8493624bf6978fe24c48389b
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-05-04 15:25:08 +0100

    web flasks

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return render_template("10-hbnb_filters.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
