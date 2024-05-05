38ea70f1f18cefe86497335c4e84121fbfbe890a
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-05-05 19:27:45 +0000

    corrections

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-""" This module starts a Flask web application
 """
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """ The "cities_by_states" route.
-        Sends a corresponding template.
-    """
-    states = storage.all(State).values()
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """ Hook that runs before app would be closed
-    """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
