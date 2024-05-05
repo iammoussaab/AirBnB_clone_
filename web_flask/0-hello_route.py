9751da16f477a40f7e98f38e574cdc64389bec0a
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-05-05 19:05:16 +0000

    replace things

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""A script that starts a flask web application
-Your web application must be listening on 0.0.0.0, port 5000
 """
-from flask import Flask, render_template
+starts a Flask web application
+"""
 
-app = Flask("__name__")
+from flask import Flask
+app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return render_template("10-hbnb_filters.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
